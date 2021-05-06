58d6284a54c4f75fcd45d917ec875bd149403464
James Leith <james@Jamess-MacBook-Pro-2.local>
2021-05-06 10:26:13 -0700

    small styling changes

---
+++ code-blog.API/Data/Seed.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using code_blog.API.Models;
+using Newtonsoft.Json;
+
+namespace code_blog.API.Data
+{
+    public class Seed
+    {
+         public static void SeedUsers(DataContext context) 
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (!context.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText(path + @"/Data/UserSeedData.json");
+
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                foreach (var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+                    context.Users.Add(user);
+                }
+
+                context.SaveChanges();
+            }
+            
+        }
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512()) 
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+    }
+}
--- code-blog.API/Program.cs
+++ code-blog.API/Program.cs
@@ -1,4 +1,9 @@
+using System;
+using code_blog.API.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace code_blog.API
@@ -7,14 +12,29 @@ namespace code_blog.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedUsers(context);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }   
+            }
+
+            host.Run();
+
         }
 
-       public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+       public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+                
     }
 }
